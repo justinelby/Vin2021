e72a21eb9186026dfd1f4b82114c110d51f77070
Justine loubry <justineloubry@gmail.com>
2021-02-08 10:19:12 +0100

    Initial Commit

---
+++ Vin21/Vin21App.swift
@@ -0,0 +1,17 @@
+//
+//  Vin21App.swift
+//  Vin21
+//
+//  Created by Justine Loubry on 08/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct Vin21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
